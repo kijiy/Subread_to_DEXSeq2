1835f4654b7ac7a844a21123b30abb2bf9206390
Yusuke Kijima <kijima@sympathyforthedevil.molgen.mpg.de>
2024-11-01 16:48:43 +0100

    update

--- dexseq_prepare_annotation_intron.py
+++ dexseq_prepare_annotation_intron.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, collections, itertools, os.path, optparse, os # changed here to add sed
+import sys, collections, itertools, os.path, optparse, os, copy, pprint # changed here to add sed
 
 optParser = optparse.OptionParser(
 
@@ -19,8 +19,8 @@ optParser = optparse.OptionParser(
 	"Modified by Vivek Bhardwaj (just a bit!) to write featurecounts gtf as an option.")
 
 optParser.add_option( "-r", "--aggregate", type="choice", dest="aggregate",
-   choices = ( "no", "yes" ), default = "yes",
-   help = "'yes' or 'no'. Indicates whether two or more genes sharing an exon should be merged into an 'aggregate gene'. If 'no', the exons that can not be assiged to a single gene are ignored." )
+   choices = [ "yes" ], default = "yes",
+   help = "Always aggregate genes (leaving this option argument for the legacy purpose). Indicates whether two or more genes sharing an exon should be merged into an 'aggregate gene'. If 'no', the exons that can not be assiged to a single gene are ignored." )
 
 # add option for featurecounts output
 optParser.add_option( "-f", "--featurecountsgtf", type="string", dest="fcgtf", action = "store",
@@ -55,13 +55,15 @@ aggregateGenes = opts.aggregate == "yes"
 
 # Step 1: Store all exons with their gene and transcript ID
 # in a GenomicArrayOfSets
-
+print("Step 1: Loading GTF file...", flush = True)
 exons = HTSeq.GenomicArrayOfSets( "auto", stranded=True )
 for f in HTSeq.GFF_Reader( gtf_file ):
    if f.type != "exon":
-      continue
+         continue
    f.attr['gene_id'] = f.attr['gene_id'].replace( ":", "_" )
    exons[f.iv] += ( f.attr['gene_id'], f.attr['transcript_id'] )
+
+   
 
 
 # Step 2: Form sets of overlapping genes
@@ -71,22 +73,52 @@ for f in HTSeq.GFF_Reader( gtf_file ):
 # The keys of 'gene_sets' are the IDs of all genes, and each key refers to
 # the set that contains the gene.
 # Each gene set forms an 'aggregate gene'.
+print("Step 2: Storing overlapping gene information...", flush = True)
+is_next_region_intron = dict()
+prev_gene_set = set()
+prev_region = ""
 
-if aggregateGenes == True:
-   gene_sets = collections.defaultdict( lambda: set() )
-   for iv, s in exons.steps():
-      # For each step, make a set, 'full_set' of all the gene IDs occuring
-      # in the present step, and also add all those gene IDs, whch have been
-      # seen earlier to co-occur with each of the currently present gene IDs.
-      full_set = set()
-      for gene_id, transcript_id in s:
-         full_set.add( gene_id )
-         full_set |= gene_sets[ gene_id ]
-      # Make sure that all genes that are now in full_set get associated
-      # with full_set, i.e., get to know about their new partners
-      for gene_id in full_set:
-         assert gene_sets[ gene_id ] <= full_set
-         gene_sets[ gene_id ] = full_set
+# if aggregateGenes == True: # Regacy: aggregateGenes is always True in this code
+gene_sets = collections.defaultdict( lambda: set() )
+for cnt, (iv, s) in enumerate(exons.steps()):
+   # For each step, make a set, 'full_set' of all the gene IDs occuring
+   # in the present step, and also add all those gene IDs, whch have been
+   # seen earlier to co-occur with each of the currently present gene IDs.
+   full_set = set()
+   for gene_id, transcript_id in s:
+      full_set.add( gene_id )
+      full_set |= gene_sets[ gene_id ]
+      
+   # Make sure that all genes that are now in full_set get associated
+   # with full_set, i.e., get to know about their new partners
+   for gene_id in full_set:
+      assert gene_sets[ gene_id ] <= full_set
+      gene_sets[ gene_id ] = full_set
+
+   # Judge if the previous exon was followed by intron.
+   # This can be told by checking if the current and previous
+   # gene ids have overlaps.
+   if len(s) > 0: # Exon
+      if len(full_set & prev_gene_set) > 0:
+         is_next_region_intron[prev_region] = True
+      else:
+         is_next_region_intron[prev_region] = False
+      
+      # Store the prev info
+      prev_gene_set = copy.copy(full_set)
+      prev_region = copy.copy(iv)
+
+   
+   if cnt == 100:
+      break
+
+# The final exon in the entire GTF shouldn't have introns after it
+is_next_region_intron[prev_region] = False
+
+pprint.pprint(gene_sets)
+print(is_next_region_intron)
+
+exit()
 
 
 # Step 3: Go through the steps again to get the exonic sections. Each step
@@ -97,29 +129,38 @@ if aggregateGenes == True:
 # aggregate ID, a list of all its exonic_part features.
 
 aggregates = collections.defaultdict( lambda: list() )
-for iv, s in exons.steps( ):
-   # Skip empty steps
+gene_id_prev = ""
+for cnt, (iv, s) in enumerate(exons.steps()):
+   # Deal with non-exonic region
    if len(s) == 0:
-      continue
-   gene_id = list(s)[0][0]
-   ## if aggregateGenes=FALSE, ignore the exons associated to more than one gene ID
-   if aggregateGenes == False:
-      check_set = set()
-      for geneID, transcript_id in s:
-         check_set.add( geneID )
-      if( len( check_set ) > 1 ):
-         continue
+      if iv in is_next_region_intron:
+         gene_id = "to be filled"
       else:
-         aggregate_id = gene_id
-   # Take one of the gene IDs, find the others via gene sets, and
-   # form the aggregate ID from all of them
-   else:
-      assert set( gene_id for gene_id, transcript_id in s ) <= gene_sets[ gene_id ]
-      aggregate_id = '+'.join( gene_sets[ gene_id ] )
+         continue
+      
+   gene_id = list(s)[0][0]
+
+   ## if aggregateGenes=FALSE, ignore the exons associated to more than one gene ID
+   ## This option does not exist in the current implementation
+
+   # if aggregateGenes == False:
+   #    check_set = set()
+   #    for geneID, transcript_id in s:
+   #       check_set.add( geneID )
+   #    if( len( check_set ) > 1 ):
+   #       continue
+   #    else:
+   #       aggregate_id = gene_id
+
+   # # Take one of the gene IDs, find the others via gene sets, and
+   # # form the aggregate ID from all of them
+   # else:
+   assert set( gene_id for gene_id, transcript_id in s ) <= gene_sets[ gene_id ]
+   aggregate_id = '+'.join( gene_sets[ gene_id ] )
+   
    # Make the feature and store it in 'aggregates'
    f = HTSeq.GenomicFeature( aggregate_id, "exonic_part", iv )
    f.source = os.path.basename( sys.argv[0] )
-#   f.source = "camara"
    f.attr = {}
    f.attr[ 'gene_id' ] = aggregate_id
    transcript_set = set( ( transcript_id for gene_id, transcript_id in s ) )
